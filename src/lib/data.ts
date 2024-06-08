1cc57ab87d776522376be7415604d5f472272ce4
Jun Xia <xiaj8@uci.edu>
2024-06-07 18:53:12 -0700

    Reorganized the elements of table

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,58 +1,11 @@
-export const data: Info[] = [
-    {
-      id: "0001201633",
-      amount: 316,
-      name: "Samueli, Henry",
-      CIK: "0001201633",
-      Company: "Random Company",
-      forms: "4, 8-k, 10-k",
-      date: "2023-03-01"
-    },
-    {
-      id: "0000000001",
-      amount: 242,
-      name: "A",
-      CIK: "0000000001",
-      Company: "Random Company",
-      forms: "4",
-      date: "2023-03-01"
-    },
-    {
-      id: "0000000002",
-      amount: 837,
-      name: "B",
-      CIK: "0000000002",
-      Company: "Random Company",
-      forms: "4",
-      date: "2023-03-01"
-    },
-    {
-      id: "0000000003",
-      amount: 874,
-      name: "C",
-      CIK: "0000000003",
-      Company: "Random Company",
-      forms: "4",
-      date: "2023-03-01"
-    },
-    {
-      id: "0000000004",
-      amount: 721,
-      name: "D",
-      CIK: "0000000004",
-      Company: "Random Company",
-      forms: "4",
-      date: "2023-03-01"
-    },
-  ]
-  
-  export type Info = {
-    id: string;
-    amount: number;
-    name: string;
-    CIK: string;
-    Company: string;
-    forms: string;
-    date: string;
-  };
-  
+
+export type Info = {
+  id: string;
+  amount: number;
+  name: string;
+  CIK: string;
+  Company: string;
+  forms: string;
+  date: string;
+  status: string;
+};
